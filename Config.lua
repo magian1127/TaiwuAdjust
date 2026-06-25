586b885f0594dcf55d2c5c275ba4d4b5e3bc418a
Magian <zang_magian@163.com>
2026-06-25 14:40:55 +0800

    上传创意工坊

--- Config.lua
+++ Config.lua
@@ -1,6 +1,6 @@
 return {
 	Title = "综合调整",
-	FileId = 0,
+	FileId = 3751327764,
 	Description = "太吾绘卷综合调整。\n\n当前功能：\n查看人物背包书籍时显示该人物的阅读完成状态。\n制造装备资源自动填充。\n\n后续调整看我玩游戏的情况会陆续添加。",
 	Cover = "Cover.jpg",
 	Author = "Magian",
@@ -13,21 +13,21 @@ return {
 	Source = 1,
 	GameVersion = "1.0.20.0",
 	DefaultSettings = {
-		{
+		[1] = {
 			SettingType = "Toggle",
 			Key = "BookReadStatus",
 			DisplayName = "人物书籍阅读状态",
 			Description = "查看人物背包书籍时显示该人物的阅读完成状态",
 			DefaultValue = true,
 		},
-		{
+		[2] = {
 			SettingType = "Toggle",
 			Key = "AutoFillCraftMaterial",
 			DisplayName = "制造自动填充资源",
 			Description = "制造装备选好材料后自动填满资源",
 			DefaultValue = true,
 		},
-		{
+		[3] = {
 			SettingType = "Toggle",
 			Key = "DebugMode",
 			DisplayName = "调试模式",
@@ -39,10 +39,9 @@ return {
 	TagList = {
 		[1] = "Optimizations",
 	},
-	WorkshopCover = nil,
+	WorkshopCover = "Cover.jpg",
 	Visibility = 0,
-	UpdateLogList = { },
 	ChangeConfig = false,
 	HasArchive = false,
-	NeedRestartWhenSettingChanged = false,
+	NeedRestartWhenSettingChanged = false
 }
